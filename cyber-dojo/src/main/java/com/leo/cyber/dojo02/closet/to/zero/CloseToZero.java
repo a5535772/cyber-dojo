32144406b7d9f896f393b3d6b066e9c46d07e906
a5535772 <214529283@qq.com>
2018-07-23 19:58:34 +0800

    增加实现

--- cyber-dojo/src/main/java/com/leo/cyber/dojo02/closet/to/zero/CloseToZero.java
+++ cyber-dojo/src/main/java/com/leo/cyber/dojo02/closet/to/zero/CloseToZero.java
@@ -2,4 +2,50 @@ package com.leo.cyber.dojo02.closet.to.zero;
 
 public class CloseToZero {
 
+	public int chose(int[] sourceArray) {
+		Integer minPositiveNum = null;
+		Integer maxNegativeNum = null;
+		for (int num : sourceArray) {
+			if (num == 0) {
+				return 0;
+			}
+			maxNegativeNum = choseMaxNegativeNum(maxNegativeNum, num);
+			minPositiveNum = choseMinPositiveNum(minPositiveNum, num);
+		}
+		return choseClosest(minPositiveNum, maxNegativeNum);
+	}
+
+	private Integer choseMinPositiveNum(Integer minPositiveNum, int num) {
+		if (num > 0) {
+			if (minPositiveNum == null) {
+				minPositiveNum = num;
+			} else {
+				minPositiveNum = minPositiveNum < num ? minPositiveNum : num;
+			}
+		}
+		return minPositiveNum;
+	}
+
+	private Integer choseMaxNegativeNum(Integer maxNegativeNum, int num) {
+		if (num < 0) {
+			if (maxNegativeNum == null) {
+				maxNegativeNum = num;
+			} else {
+				maxNegativeNum = maxNegativeNum > num ? maxNegativeNum : num;
+			}
+		}
+		return maxNegativeNum;
+	}
+
+	private Integer choseClosest(Integer minPositiveNum, Integer maxNegativeNum) {
+		if (minPositiveNum == null)
+			return maxNegativeNum;
+		if (maxNegativeNum == null)
+			return minPositiveNum;
+		if(Math.abs(maxNegativeNum)<minPositiveNum) {
+			return maxNegativeNum;
+		}
+		return minPositiveNum;
+	}
+
 }
---
+++ cyber-dojo/src/test/java/com/leo/cyber/dojo02/closet/to/zero/CloseToZeroTest.java
@@ -0,0 +1,32 @@
+package com.leo.cyber.dojo02.closet.to.zero;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class CloseToZeroTest {
+	CloseToZero closeToZero = new CloseToZero();
+
+	static final int ZERO = 0;
+
+	@Test
+	public void should_return_zero_when_zero_exists() {
+		final int[] sourceArray = { 0, 1 };
+		int closest = closeToZero.chose(sourceArray);
+		assertThat(closest).isEqualTo(ZERO);
+	}
+
+	@Test
+	public void should_return_min_value_when_no_num_is_less_than_zero() {
+		final int[] sourceArray = { 9, 88, 71, 2, 3, 5 };
+		int closest = closeToZero.chose(sourceArray);
+		assertThat(closest).isEqualTo(2);
+	}
+
+	@Test
+	public void should_return_max_value_when_no_num_is_bigger_than_zero() {
+		final int[] sourceArray = { -9, -88, -71, -2, -3, -5 };
+		int closest = closeToZero.chose(sourceArray);
+		assertThat(closest).isEqualTo(-2);
+	}
+}
